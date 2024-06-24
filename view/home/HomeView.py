abf8c731312168687c64a43eb8f92adbb116fe64
CCIDDIGGI <marco.boccolacci@gmail.com>
2024-06-24 10:44:45 +0200

    added difference value validation

--- view/home/HomeView.py
+++ view/home/HomeView.py
@@ -3,19 +3,21 @@ from customtkinter import *
 from custom.CTkScrollableDropdown import CTkScrollableDropdown 
 
 class HomeView(CTkFrame):
-    
+    diff_value = 0
     # child of MainView -> parent argument is MainView
     def __init__(self, parent):
         # main setup
         super().__init__(parent)
         self.place(relx = 0.251, rely = 0, relwidth = 0.75, relheight = 1)
         self.radio_diff_var = tkinter.IntVar(value=1)
+        self.entry_diff_var = tkinter.StringVar()
 
         # widgets
         lbl_diff = CTkLabel(self, width=300, text="Minimum price difference between the first and second cheapest listings")
         radio_diff_perc = CTkRadioButton(self, text="Percentage", variable=self.radio_diff_var, value=1)
         radio_diff_flat = CTkRadioButton(self, text="Flat value", variable=self.radio_diff_var, value=2)
-        self.entry_diff = CTkEntry(self, width=150)
+        self.entry_diff = CTkEntry(self, width=150, textvariable=self.entry_diff_var)
+        self.lbl_diff_err = CTkLabel(self, width=300, text="Please insert only numerical values!")
         self.entry_exp = CTkEntry(self, width=300)
         btn_exp = CTkButton(self, text = "Get all listings for current expansion", state="disabled", command=self.get_listings_by_exp_id)
         self.scr_drpd = CTkScrollableDropdown(self.entry_exp,
@@ -25,13 +27,16 @@ class HomeView(CTkFrame):
                                                   or btn_exp.configure(state="normal"),
                                               autocomplete=True) 
         
+        # widgets callback
+        self.entry_diff_var.trace_add("write", self.try_parse_diff_var)
+        
         # widgets rendering
         lbl_diff.place(x=100, y=50)
         radio_diff_perc.place(x=100, y=100)
         radio_diff_flat.place(x=250, y=100)
         self.entry_diff.place(x=100, y=150)
-        btn_exp.place(x=400, y=250)
-        self.entry_exp.place(x=100, y=250)
+        self.entry_exp.place(x=100, y=200)
+        btn_exp.place(x=400, y=200)
 
     def set_controller(self, controller) -> None:
         self.controller = controller
@@ -40,6 +45,14 @@ class HomeView(CTkFrame):
     def initialize_variables(self) -> None:
         self.exp_list = self.controller.get_expansions_list()
         self.scr_drpd.configure(values=self.exp_list)   
+
+    def try_parse_diff_var(self, *args) -> None:
+        try:
+            if not (not self.entry_diff_var.get()):
+                self.diff_value = int(self.entry_diff_var.get())
+            self.lbl_diff_err.place_forget()
+        except ValueError:
+            self.lbl_diff_err.place(x=250, y=150)
 
     def get_listings_by_exp_id(self) -> None:
         exp_id = self.controller.get_exp_id_by_exp_name(self.entry_exp.get())
